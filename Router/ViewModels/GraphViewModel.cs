c973ad9b6690f9875111f5863eae451f4aa7b4aa
dandriano <dandrianov@outlook.com>
2023-10-31 13:42:18 +0300

    Add proper connection point styling for better "connectivity" with link

--- Router/ViewModels/GraphViewModel.cs
+++ Router/ViewModels/GraphViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GraphX.Common.Enums;
 using GraphX.Controls;
+using GraphX.Controls.Models;
 using GraphX.Logic.Models;
 using Router.Model;
 using System.Windows;
@@ -29,25 +30,35 @@ namespace Router.ViewModels
             SetEdgesHighlight(true, GraphControlType.VertexAndEdge);
             ShowAllEdgesArrows(true);
 
+            VertexDoubleClick += ProceedVertexPointDoubleClick;
+            // Uncomment for pre-creation of node pairs with link
             /*
             var n1 = AddNode(1, "#1 Node", new Point(25, 25));
             var n2 = AddNode(2, "#2 Node", new Point(100, 100));
+            // var n3 = AddNode(3, "#3 Node", new Point(200, 200));
             var e = AddLink(n1, n2);
             */
+            
+        }
+
+        private void ProceedVertexPointDoubleClick(object sender, VertexSelectedEventArgs args)
+        {
+            var nodeControl = args.VertexControl;
+            var point = nodeControl.GetConnectionPointAt(args.MouseArgs.GetPosition(this));
+
+            if (point != null)
+            {
+                // TODO: Add dynamic link creation on dblclick
+            }
         }
 
         public Node AddNode(long id, string name, Point pos)
         {
             var node = new Node(id, name);
             var nodeControl = new VertexControl(node);
-            /*
-            var vcp = new StaticVertexConnectionPoint() { Id = 1 };
-            var ctl = new Border() { Child = vcp };
-            nodeControl.VCPRoot.Children.Add(ctl);
-            nodeControl.VertexConnectionPointsList.Add(vcp);
-            */
             nodeControl.SetPosition(pos);
             AddVertexAndData(node, nodeControl);
+            nodeControl.OnApplyTemplate();
 
             return node;
         }
@@ -56,8 +67,11 @@ namespace Router.ViewModels
         {
             var sourceControl = VertexList[source];
             var targetControl = VertexList[target];
-
-            var link = new Link(source, target, weight);
+            var link = new Link(source, target, weight)
+            {
+                SourceConnectionPointId = 1,
+                TargetConnectionPointId = 1
+            };
             var linkControl = new EdgeControl(sourceControl, targetControl, link);
             AddEdgeAndData(link, linkControl, true);
 
