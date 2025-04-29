94d2e1c0292faea41986d0008389c203f53386e0
dandriano <dandrianov@outlook.com>
2025-04-29 07:51:21 +0300

    Naive grid drawing implementation

--- Router.Wasm/NetworkRenderer.cs
+++ Router.Wasm/NetworkRenderer.cs
@@ -1,23 +1,26 @@
 ﻿using Blazorex;
+using Router.Wasm.Extensions;
 
 namespace Router.Wasm
 {
     /// <summary>
-    /// A basic renderer for a bidirectional graph (make generic someday?)
+    /// A basic renderer for a bidirectional graph
     /// </summary>
+    /// <remarks>
+    /// Make generic someday? And also generic <see cref="NetworkSettings"/>.
+    /// </remarks>
     public class NetworkRenderer
     {
-        private NetworkSettings _settings;
+        private readonly NetworkSettings _settings;
 
         public NetworkRenderer(NetworkSettings settings)
         {
             _settings = settings;
         }
-
         public void Render(IRenderContext ctx, Network network)
         {
             // TODO: to implement (as extensions to IRenderContext or as private methods right here..)
-            // ctx.DrawGrid(_settings);
+            ctx.DrawGrid(_settings);
             // ctx.DrawVerticies(network.Verticies)
             // ctx.DrawEdges(network.Edges)
         }
